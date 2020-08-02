fe705e19d62a8994061f90a2c314279417b4eb76
AtharvRedij <atharvaredij@gmail.com>
2020-08-02 11:37:30 +0530

    Updates

---
+++ precache-manifest.f0dfd347583754de05fbe6be50ef1e7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6191623602cacc91f929621accb408c0",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "325c6427139c61b6b718",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "389f528d8e34a7179132",
+    "url": "/covid-19-tracker/static/css/main.b84bdfbf.chunk.css"
+  },
+  {
+    "revision": "325c6427139c61b6b718",
+    "url": "/covid-19-tracker/static/js/2.9704e6e4.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-tracker/static/js/2.9704e6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389f528d8e34a7179132",
+    "url": "/covid-19-tracker/static/js/main.8514fb5f.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
